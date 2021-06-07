efcac5194056f7e23ceaca2807816484bbdfeed5
Anshuman singh <55177161+Anshu32@users.noreply.github.com>
2021-06-07 15:30:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zfhxtzlngqgkmy:94b62158ebb4c4041ec860fc9880b03b60b45448f83ef5f6ee251914d892e763@ec2-3-226-134-153.compute-1.amazonaws.com:5432/da8dtqsg04qhsh'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zfhxtzlngqgkmy:94b62158ebb4c4041ec860fc9880b03b60b45448f83ef5f6ee251914d892e763@ec2-3-226-134-153.compute-1.amazonaws.com:5432/da8dtqsg04qhsh'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
